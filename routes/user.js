834f2eed7910b071a86b55224839e4efc54edb60
Tasha Mitchell <tamitchell14@gmail.com>
2018-08-11 11:03:36 -0400

    added todo list

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.js');
-const profileController = require('../controllers/profile')
+// const profileController = require('../controllers/profile')
 
 
 router.get('/login', userController.login);
@@ -12,7 +12,7 @@ router.get('/logout', userController.logout);
 
 router.post("/:id", profileController.createStat);
 router.get("/stats/new", profileController.newStat);
-router.get("/:id", profileController.showStats);
+// router.get("/:id", profileController.showStats);
 // router.get("/:id/stats/edit", profileController.editStat)
 // router.put("/:id/stats/update", profileController.updateStat);
 
